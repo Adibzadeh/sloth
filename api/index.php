631cd369bd5567e17d5926a7f80f64882a2e5868
Hamid Adibzadeh <hamid0x4c@gmail.com>
2023-03-24 16:14:14 +0330

    Basic project structure

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php
+// return all routes based on user privileges
+
+
---
+++ install.php
@@ -0,0 +1,14 @@
+<?php
+// Please delete or change its name and take a backup after completing the installation
+
+// Website Name
+
+// Admin Username
+
+// Admin Password
+
+// Admin panel url
+
+// Database type and access details
+
+// Other settings
